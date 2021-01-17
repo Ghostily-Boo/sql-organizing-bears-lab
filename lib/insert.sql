0038e7822942ec892a7beadbcc0e595e75049239
Ghostily-Boo <grace.rl.qin@gmail.com>
2021-01-17 14:01:20 -0500

    commit tree e7fd875c4078c1a1ce6723f1fe01b1d51f7e9cd5

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES 
+        ('Mr. Chocolate', 20, 'F', 'Brown', 'Docile', true),
+        ('Rowdy', 12, 'M', 'White', 'Insane', true),
+        ('Tabitha', 11, 'F', 'Blue-Gray', 'Maternal', true),
+        ('Seargent Brown', 19, 'F', 'Black', 'Aggressive', false),
+        ('Melissa', 2, 'F', 'Brown', 'Social', true),
+        ('Grinch', 24, 'M', 'Blond', 'Anti-Social', true),
+        ('Wendy', 9, 'F', 'Brown', 'Docile', false),
+        (NULL, 2, 'M', 'Brown', 'Playful', true);
